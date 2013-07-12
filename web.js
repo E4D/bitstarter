6c7d9bb4da83b122cebb54c339fdc223115d7643
Ubuntu <ubuntu@ip-172-31-20-121.us-west-2.compute.internal>
2013-07-12 00:34:28 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
+// read index.html into a buffer this should be done async in the spirit of node but...
+var fileBuffer=new Buffer(fs.readFileSync('index.html'));
+
+// request for the root return the index page
 app.get('/', function(request, response) {
-  fs.readFile('index.html', 'utf8', function (err,data) {
-     if (err) {
-       return console.log(err);
-     }
-     response.send(data);
-  });
- });
+  response.send(fileBuffer.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
